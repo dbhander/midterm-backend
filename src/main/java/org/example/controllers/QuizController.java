c256904fea8f9b056a845e2af6592391006129e1
dbhander <dbhander@iu.edu>
2025-03-16 23:36:48 -0400

    small change

--- src/main/java/org/example/controllers/QuizController.java
+++ src/main/java/org/example/controllers/QuizController.java
@@ -13,7 +13,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
-@CrossOrigin(origins = "https://dbhander.github.io/midterm-frontend/")
+@CrossOrigin(origins = "https://dbhander.github.io/")
 @RestController
 @RequestMapping("/quizzes")
 public class QuizController {
--- src/main/java/org/example/controllers/QuizQuestionController.java
+++ src/main/java/org/example/controllers/QuizQuestionController.java
@@ -11,7 +11,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 import java.util.List;
 
-@CrossOrigin(origins = "https://dbhander.github.io/midterm-frontend/")
+@CrossOrigin(origins = "https://dbhander.github.io/")
 @RestController
 @RequestMapping("/questions")
 public class QuizQuestionController {
