7af12f8a4c51e2750e0f013775cd5d90a1e06cd2
dbhander <dbhander@iu.edu>
2025-03-22 18:08:14 -0400

    url changes

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,7 +1,9 @@
 package org.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin(origins = "https://dbhander.github.io/midterm-frontend/")
 @SpringBootApplication
 public class App 
 {
